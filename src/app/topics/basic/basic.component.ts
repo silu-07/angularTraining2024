be0d542b978bda0792ba7ac86aea27a83ca0ec73
silu-07 <90642615+silu-07@users.noreply.github.com>
2024-05-28 18:35:38 +0530

    Initital Build..

--- src/app/topics/basic/basic.component.ts
+++ src/app/topics/basic/basic.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatListModule} from '@angular/material/list';
 
 @Component({
   selector: 'app-basic',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatListModule],
   templateUrl: './basic.component.html',
   styleUrl: './basic.component.scss'
 })
-export class BasicComponent {
 
+
+
+export class BasicComponent {
+  test:any;
 }
